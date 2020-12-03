3ce3520662d4e54660570a0d1489560b9a4101ec
a63933 <1324147533@qq.com>
2020-12-03 14:53:56 +0800

    x

---
+++ index.js
@@ -0,0 +1,27 @@
+#!/usr/bin/env node
+
+const path = require('path');
+const fs = require('fs');
+const inquirer = require('inquirer');
+const ejs = require('ejs');
+
+inquirer.prompt([
+  {
+    type: 'input',
+    name: 'title',
+    message: 'Project name'
+  }
+]).then(answers => {
+  const tmplDir = path.join(__dirname, 'templates')
+  const destDir = process.cwd()
+
+  fs.readdir(tmplDir, (err, files) => {
+    if(err) throw err
+
+    files.forEach(file => {
+      ejs.renderFile(path.join(tmplDir, file), answers, (err, result) =>{
+        fs.writeFileSync(path.join(destDir, file), result)
+      })
+    })
+  })
+})
